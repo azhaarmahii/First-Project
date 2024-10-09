2e2c7d86536d868065aad33a757151ebf33efbf9
azhaarmahii <azhaarmahii88@gmail.com>
2024-10-09 08:48:07 -0700

    we made login page

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, View } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+const { height, width } = Dimensions.get("screen");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Image
-        source={require("./assets/src/images/pak.webp")}
-        style={styles.img}
-      />
+      <Text style={styles.heading}>Login</Text>
+      <View>
+        <TextInput
+          placeholder="Enter your name"
+          style={styles.input}
+          inputMode="email"
+        />
+        <TextInput
+          placeholder="Enter your password"
+          style={[styles.input, { marginTop: 20 }]}
+          inputMode="none"
+          secureTextEntry
+        />
+
+        <TouchableOpacity
+          style={{
+            height: 48,
+            width: "90%",
+            backgroundColor: "blue",
+            alignSelf: "center",
+            marginTop: 50,
+            borderRadius: 44,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#fff" }}>Login </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -17,14 +49,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    // justifyContent: "center",
   },
-  img: {
-    flex: 1,
-    marginTop: 100,
-    borderRadius: 99,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    // textAlign: "center",s
+    textAlign: "center",
+    marginTop: height * 0.1,
+  },
+  input: {
+    borderWidth: 1,
+    height: 48,
+    width: "90%",
+    alignSelf: "center",
+    paddingLeft: 10,
+    borderRadius: 12,
+    marginTop: height * 0.05,
   },
 });
